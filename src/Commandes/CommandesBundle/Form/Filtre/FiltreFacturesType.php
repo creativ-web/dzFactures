2857f3eb571655ad295f5c3e6f5d85f40815cf1b
Mehdi Boumediene <mehdi@creativ-dz.com>
2019-10-31 12:20:34 -0400

    ok

--- src/Commandes/CommandesBundle/Form/Filtre/FiltreFacturesType.php
+++ src/Commandes/CommandesBundle/Form/Filtre/FiltreFacturesType.php
@@ -77,7 +77,7 @@ class FiltreFacturesType extends AbstractType
                     'Envoyé' => 'Envoyé',
                     'Impayé' => 'Impayé',
                 ),
-                'data' => "Créé",'required'=>true))
+                'data' => "tous",'required'=>true))
             ->add('zone', EntityType::class, array(
                 'class' => 'Commandes\CommandesBundle\Entity\Zonnestocks',
                 'query_builder' => function (EntityRepository $er) {
